f0d65553a45d78bcdcd037ead33786873dd3324e
tt'ct'c'y田宸宇 <tianchy3@mail2.sysu.edu.cn>
2018-10-15 15:56:57 +0800

    07-图6 旅游规划
    输入格式:
    输入说明：输入数据的第1行给出4个正整数N、M、S、D，其中N（2≤N≤500）是城市的个数，顺便假设城市的编号为0~(N−1)；M是高速公路的条数；S是出发地的城市编号；D是目的地的城市编号。随后的M行中，每行给出一条高速公路的信息，分别是：城市1、城市2、高速公路长度、收费额，中间用空格分开，数字均为整数且不超过500。输入保证解的存在。
    
    输出格式:
    在一行里输出路径的长度和收费总额，数字间以空格分隔，输出结尾不能有多余空格。
    
    输入样例:
    4 5 0 3
    0 1 1 20
    1 3 2 30
    0 3 4 10
    0 2 2 20
    2 3 1 20
    输出样例:
    3 40

--- week7/week7_task3.py
+++ week7/week7_task3.py
@@ -1,1 +1,57 @@
-#旅游规划
+# 旅游规划
+def Find_min():
+    min_index = -1
+    min_miles = 99999
+    min_cost = 99999
+    for i in range(N):
+        if visited[i] == 0:
+            if miles[i] < min_miles:
+                min_index = i
+                min_miles = miles[i]
+                min_cost = cost[i]
+            elif (miles[i] == min_miles) and (cost[i] < min_cost):
+                min_index = i
+                min_cost = cost[i]
+            else:
+                continue
+    return min_index
+
+
+N, M, S, D = [int(x) for x in input().split()]
+#  N:#cities   M:#railways  S:id of origins  D:id of destinations
+# 可以看成是单源最短路问题
+# 利用邻接表
+network = []
+visited = []
+cost = []
+miles = []
+for i in range(N):
+    network.append([])
+    miles.append(99999)
+    cost.append(99999)
+    visited.append(0)
+visited[S] = 1
+cost[S] = 0
+miles[S] = 0
+
+for i in range(M):
+    ori, des, lth, fee = [int(x) for x in input().split()]
+    network[ori].append([des, lth, fee])
+    network[des].append([ori, lth, fee])
+now=S
+while now!=D:
+
+    visited[now] = 1  # 表示已经访问过
+    for route in network[now]:
+        des, lth, fee = route
+        if visited[des]==0:
+            if miles[des] > miles[now] + lth:
+                miles[des] = miles[now] + lth
+                cost[des] = cost[now] + fee
+            elif miles[des] == (miles[now]+lth) and cost[des]>(cost[now]+fee):
+                cost[des] = cost[now]+fee
+            else:
+                continue
+    now = Find_min()
+
+print(miles[D],cost[D])
