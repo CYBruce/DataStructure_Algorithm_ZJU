89aa06efa26529a3b80865f887dd324c22f542ca
tt'ct'c'y田宸宇 <tianchy3@mail2.sysu.edu.cn>
2018-11-11 15:09:21 +0800

    week11

---
+++ week11/week11_task1.py
@@ -0,0 +1,41 @@
+#11-散列1 电话聊天狂人
+def ScanAndOutput(hashtable):
+    MinPhone,MaxCnt,PCnt=(0,0,0)
+    for i in range(len(hashtable)):
+        Ptr = hashtable[i]
+        if len(Ptr)!=0:
+            for users in Ptr:
+                if users[1]>MaxCnt:
+                    MaxCnt=users[1]
+                    MinPhone=users[0]
+                    PCnt=1
+                elif users[1]==MaxCnt:
+                    MinPhone = min(users[0],MinPhone)
+                    PCnt += 1
+    if PCnt > 1:
+        print(MinPhone,MaxCnt,end=' ')
+        print(PCnt)
+    else:
+        print(MinPhone, MaxCnt)
+
+def CreateTable(tablesize):
+    H = [[] for i in range(N*2)]
+    return H
+
+def Insert(hash,num):
+    position = num%100000%(N*2)
+    for cell in hash[position]:
+        if num == cell[0]:
+            cell[1]+=1
+            return True
+    hash[position].append([num,1])
+
+
+#主程序框架
+N = int(input())
+H = CreateTable(N*2)
+for i in range(N):
+    number1, number2 = [int(x) for x in input().split()]
+    Insert(H,number1)
+    Insert(H,number2)
+ScanAndOutput(H)
