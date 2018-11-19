a18f41b35ae3387706e28aaa6d40d50ed358ab3b
tt'ct'c'y田宸宇 <tianchy3@mail2.sysu.edu.cn>
2018-11-19 11:00:49 +0800

    a faster method

--- 散列查找/11-散列4 Hashing - Hard Version.py
+++ 散列查找/11-散列4 Hashing - Hard Version.py
@@ -27,5 +27,36 @@ print(ans[-1])
 '''
 #利用拓扑排序来解决问题
 TSize = int(input())
-mytable = [-1]*TSize
 hash_table = [int(x) for x in input().split()]
+ConnectMap = {}
+InDgree = {}
+for ele in hash_table:
+    if ele > -1:
+        InDgree[ele] = 0
+        ConnectMap[ele] = []
+    #初始化入度表和邻接表
+for num in InDgree:
+    err = 0
+    while hash_table[(num+err)%TSize] !=num:
+        ConnectMap[hash_table[(num + err) % TSize]].append(num)
+        err+=1
+        InDgree[num]+=1
+    #构建入度表和邻接表
+queue = []
+for number in InDgree:
+    if InDgree[number]==0:
+        queue.append(number)
+ans = []
+while queue:
+    v = min(queue)
+    queue.remove(v)
+    ans.append(v)
+    for w in ConnectMap[v]:
+        InDgree[w]=InDgree[w]-1
+        if InDgree[w]==0:
+            queue.append(w)
+
+for i in range(len(ans)-1):
+    print(ans[i],end=' ')
+print(ans[-1])
+#最大N，但是只有少数值，有非-1的空位
