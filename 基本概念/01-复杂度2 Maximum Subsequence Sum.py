ce539c34e500e9c6440c3a567826c494c8532b1a
tt'ct'c'y田宸宇 <tianchy3@mail2.sysu.edu.cn>
2018-11-19 08:33:40 +0800

    problem fixed

--- 基本概念/01-复杂度2 Maximum Subsequence Sum.py
+++ 基本概念/01-复杂度2 Maximum Subsequence Sum.py
@@ -4,15 +4,19 @@ ls = [int(x) for x in input().split()] #列表推导式
 ThisSum = MaxSum = 0
 head = ls[0]
 tail = ls[num-1]
+tag=0
 for i in range(num):
+    if((ls[i] >= 0) and tag==0):
+        this_head = ls[i]
+        tag=1
     ThisSum += ls[i]
-    if((ThisSum > MaxSum)&(MaxSum == 0)):
-        head = ls[i]
     if(ThisSum > MaxSum):
         MaxSum = ThisSum
         tail = ls[i]
+        head = this_head
     elif(ThisSum < 0):
         ThisSum = 0
-if((MaxSum == 0)&(0 in ls)):
+        tag=0
+if(MaxSum == 0 and 0 in ls):
     head = tail =0
 print(str(MaxSum)+' '+str(head)+' '+str(tail))
